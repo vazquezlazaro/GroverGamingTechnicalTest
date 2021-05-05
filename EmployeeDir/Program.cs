21ea19dccca6e67c4c21b36a8893712984e5f693
Luis <luis.vazquez0651@gmail.com>
2021-05-05 16:07:07 -0400

    added employee class

--- EmployeeDir/Program.cs
+++ EmployeeDir/Program.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using Gtk;
+using System.Collections.Generic;
 
 namespace EmployeeDir
 {
     class MainClass
     {
+        static List<Employee> employees = new List<Employee>();
+        static int count = 0;
         public static void Main(string[] args)
         {
             Application.Init();
             MainWindow win = new MainWindow();
             win.Show();
+
+            employees.Add(new Employee("John Smith", "Front Desk"));
+            count++;
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("Name: {0}, Job Tile: {1}", employee.fname, employee.jobTitle);
+            }
             Application.Run();
+
+
+
+
         }
+    }
+
+    class Employee
+    {
+        public string fname;
+        public string jobTitle;
+
+        public Employee(string name, string title)
+        {
+            fname = name;
+            jobTitle = title;
+        }
+
+        public string GetName()
+        {
+            return fname;
+        }
+
+
+
+
     }
 }
