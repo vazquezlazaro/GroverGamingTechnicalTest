5a925940f11d53f97ae41aca08717778ee244c2a
Luis <luis.vazquez0651@gmail.com>
2021-05-06 09:54:04 -0400

    added update and delete buttons

--- EmployeeDir/MainWindow.cs
+++ EmployeeDir/MainWindow.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public partial class MainWindow : Gtk.Window
 {
-    int j = 0;
+    int count = 0;
     static List<EmployeeDir.Employee> employees = new List<EmployeeDir.Employee>();
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
@@ -21,12 +21,22 @@ public partial class MainWindow : Gtk.Window
     private void BtnAdd_Click(object sender, EventArgs args)
     {
         employees.Add(new EmployeeDir.Employee(this.FirstName.Text, this.jobTitle.Text));
-        j++;
+        count++;
 
         string combindedString = string.Join("\n", employees);
        
         this.label5.Text ="Name | Job Title\n" + "\n" + combindedString;
 
-        
     }
+
+    private void BtnUpdate_Click(object sender, EventArgs args)
+    {
+        this.label5.Text = "Update works";
+    }
+
+    private void BtnDelete_Click(object sender, EventArgs args)
+    {
+        this.label5.Text = "delete works";
+    }
+
 }
